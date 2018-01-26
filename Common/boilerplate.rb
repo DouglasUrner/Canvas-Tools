dda26237ff9cb6b37d24005d2720f8568027f9f4
Douglas Urner <dlu@canishe.com>
2018-01-25 23:43:13 -0800

    Refactoring & organizing only works in GET mode.

--- Common/boilerplate.rb
+++ Common/boilerplate.rb
@@ -1,35 +1,52 @@
 require 'optparse'
 
-$options = {}
+def parse_opts(&block)
+  options = {}
 
-# Defaults
-$options[:base_url] = "https://canvas.instructure.com"
+  # Defaults
+  options[:base_url] = "https://canvas.instructure.com"
+  options[:method] = :get
 
-OptionParser.new do |opts|
+  OptionParser.new do |opts|
 
-  opts.banner = "Usage: #{File.basename($0)} [options]"
+    opts.banner = "Usage: #{File.basename($0)} [options]"
 
-  opts.on("-a", "--auth-token FILE",
-          "FILE holding authentication token") do |token_file|
-    $options[:auth_token] = File.read(token_file).chop
-  end
-
-  opts.on("-c", "--course-id NUMBER",
-          "the NUMBER that identifies the course") do |course_id|
-    $options[:course_id] = course_id
-  end
-
-  opts.on("-u", "--url URL",
-          "the base URL of the Canvas instance") do |base_url|
-    if base_url !~ /^https:\/\//
-      base_url = "https://" + base_url
+    opts.on("-a", "--auth-token FILE",
+            "FILE holding authentication token") do |token_file|
+      options[:auth_token] = File.read(token_file).chop
     end
-    $options[:base_url] = base_url
-  end
 
-  opts.on("-v", "--[no-]verbose", "Be chatty...") do |v|
-    $options[:verbose] = true
-  end
+    opts.on("-c", "--course-id NUMBER",
+            "the NUMBER that identifies the course") do |course_id|
+      options[:course_id] = course_id
+    end
 
-end.parse!
+    opts.on("-m", "--method METHOD", "make a METHOD request") do |method|
+      if method == "get"
+        options[:method] =:get
+      elsif method == "post"
+        options[:method] = :post
+      else
+        # TODO: handle unsupported methods
+        # Print message and abort
+      end
+    end
 
+    opts.on("-u", "--url URL",
+            "the base URL of the Canvas instance") do |base_url|
+      if base_url !~ /^https:\/\//
+        base_url = "https://" + base_url
+      end
+      options[:base_url] = base_url
+    end
+
+    opts.on("-v", "--[no-]verbose", "Be chatty...") do |v|
+      options[:verbose] = v
+    end
+
+    block.call(opts)
+
+  end.parse!
+
+  options
+end
--- EntryTask/EntryTask.rb
+++ EntryTask/EntryTask.rb
@@ -4,23 +4,60 @@ require 'yaml'
 
 require_relative '../Common/boilerplate'
 
+# TODO: Do POST and GET
 # TODO: Create a quiz
 # TODO: Add questions
-# TODO: Assign to sections
+# TODO: Store and retrieve bell schedule
+# TODO: Store and retrieve when sections meet
+# TODO: Assign to sections and set window
 # TODO: EntryTask specific command line args
 # TODO: Check result - handle errors
+# TODO: Verbose option - clean up and generalize
 
-quiz_api = "#{$options[:base_url]}/api/v1/courses/#{$options[:course_id]}/quizzes"
+options = parse_opts do |opts|
+  opts.on("-l", "--list",
+          "retrieve a list of entry tasks for the course") do
+    options[:list] = true
+  end
+end
 
-response = RestClient.post(
-    quiz_api,
-    JSON.load(File.open(ARGV[0])),
-    {
-        authorization: "Bearer #{$options[:auth_token]}",
-        headers: {
-            accept: :json,
-        }
+# Verbosity
+pp options
+
+quiz_api = "#{options[:base_url]}/api/v1/courses/#{options[:course_id]}/quizzes"
+
+# Create the quiz
+
+response = RestClient::Request.new({
+    method: options[:method],
+    url: quiz_api,
+    # if options[:method] == :post
+    #   JSON.load(File.open(ARGV[0])),
+    # end
+    headers: {
+        accept: :json,
+        authorization: "Bearer #{options[:auth_token]}",
     }
-)
+}).execute do |response, request, result|
+  # Verbosity
+  pp request
+  case response.code
+  when 200
+    [ :success, JSON.parse(response.to_str) ]
+  else
+    fail "#{response.to_str}"
+  end
+end
 
-pp JSON.parse(response.to_s)
+# When creating there should only be one quiz returned, but when listing
+# there will usually be many.
+
+quizzes = response[1]
+
+# Add questions
+
+quizzes.each do |quiz|
+  print "#{quiz['id']} : #{quiz['title']}\n"
+end
+
+# Check if course has sections and assign entry tasks
