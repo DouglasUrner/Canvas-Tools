5e78b55a4a28870747ff6a1a32a2495414e3c0f8
Douglas Urner <dlu@canishe.com>
2018-01-28 11:44:37 -0800

    Cleanup per Rubocop and starting on YARD docs.

--- common/util.rb
+++ common/util.rb
@@ -1,30 +1,36 @@
 require 'optparse'
 
+##
+# parse_opts() - process command line options
+#
+# @param(block) - options specific to the calling tool.
+# @return() - an array of options
+#
 def parse_opts(&block)
   options = {}
 
   # Defaults
-  options[:base_url] = "https://canvas.instructure.com"
+  options[:base_url] = 'https://canvas.instructure.com'
   options[:method] = :get
 
   OptionParser.new do |opts|
 
     opts.banner = "Usage: #{File.basename($0)} [options]"
 
-    opts.on("-A", "--auth-token FILE",
-            "FILE holding authentication token") do |token_file|
+    opts.on('-A', '--auth-token FILE',
+            'FILE holding authentication token') do |token_file|
       options[:auth_token] = File.read(token_file).chop
     end
 
-    opts.on("-c", "--course-id NUMBER",
-            "the NUMBER that identifies the course") do |course_id|
+    opts.on('-c', '--course-id NUMBER',
+            'the NUMBER that identifies the course') do |course_id|
       options[:course_id] = course_id
     end
 
-    opts.on("-m", "--method METHOD", "make a METHOD request") do |method|
-      if method == "get"
+    opts.on('-m', '--method METHOD', 'make a METHOD request') do |method|
+      if method == 'get'
         options[:method] =:get
-      elsif method == "post"
+      elsif method == 'post'
         options[:method] = :post
       else
         # TODO: handle unsupported methods
@@ -32,15 +38,15 @@ def parse_opts(&block)
       end
     end
 
-    opts.on("-u", "--url URL",
-            "the base URL of the Canvas instance") do |base_url|
+    opts.on('-u', '--url URL',
+            'the base URL of the Canvas instance') do |base_url|
       if base_url !~ /^https:\/\//
-        base_url = "https://" + base_url
+        base_url = 'https://' + base_url
       end
       options[:base_url] = base_url
     end
 
-    opts.on("-v", "--[no-]verbose", "Be chatty...") do |v|
+    opts.on('-v', '--[no-]verbose', 'Be chatty...') do |v|
       options[:verbose] = v
     end
 
